8615c1c7f1deb83eed586a34de347267961bddfc
AllanDeyvison <allandeyvison007dias@hotmail.com>
2021-04-20 19:15:29 -0300

    Bloco 1

--- Exercicios Bloco 2/MySQL 20-04-21/alunes.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM registroescolar.alunes;
-
-SELECT * FROM registroescolar.alunes WHERE nota > 7;
-SELECT * FROM registroescolar.alunes WHERE nota < 7;
-
-UPDATE `registroescolar`.`alunes` SET `nota` = '6.5' WHERE (`idAlunes` = '2');
